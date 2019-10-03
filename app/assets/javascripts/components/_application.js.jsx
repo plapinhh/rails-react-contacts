3ad174ff31dec44c966148e1dd6fac81b050930d
Pavel Lapin <pavel@trakken.de>
2019-10-03 10:58:16 +0200

    render list of contacts

--- app/assets/javascripts/components/_application.js.jsx
+++ app/assets/javascripts/components/_application.js.jsx
@@ -1,7 +1,26 @@
-const Application = (props) => {
-  return(
-    <div>
-      <h1>Contacts</h1>
-    </div>
-  )
+class Application extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('/api/v1/contacts.json')
+      .then((response) => {console.log(response);return response.json()})
+      .then((data) => {this.setState({ contacts: data }) });
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Contacts</h1>
+
+        <ContactList contacts={this.state.contacts} />
+      </div>
+    )
+  }
+
 }
---
+++ app/assets/javascripts/components/_contacts_list.js.jsx
@@ -0,0 +1,27 @@
+class ContactList extends React.Component {
+  render(){
+
+    if (!this.props.contacts.length)
+      return(
+        <div>
+          No contacts
+        </div>
+      )
+    
+    var contacts = this.props.contacts.map((contact) => {
+      return(
+        <div key={contact.id}>
+          <dt>{contact.first_name} {contact.last_name}</dt>
+          <dd>Email: {contact.email}</dd>
+          <dd>Phone: {contact.phone}</dd>
+        </div>
+      )
+    })
+
+    return(
+      <div>
+        {contacts}
+      </div>
+    )
+  }
+}
